78c85faf59f1c1f2028105b5562ac1a042d6c01e
Mehdi Charouh <71081995+SCP-OMI@users.noreply.github.com>
2023-07-20 22:58:50 +0000

    starting fresh

--- requirements/mariaDB/tools/setup-mariadb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "CREATE DATABASE IF NOT EXISTS $DB1_NAME ;" >> /etc/mysql/db1.sql
-echo "CREATE USER IF NOT EXISTS '$DB1_USER'@'%' IDENTIFIED BY '$DB1_PWD' ;" >> /etc/mysql/db1.sql
-echo "GRANT ALL PRIVILEGES ON $DB1_NAME.* TO '$DB1_USER'@'%' ;" >> /etc/mysql/db1.sql
-echo "FLUSH PRIVILEGES;" >> /etc/mysql/db1.sql
